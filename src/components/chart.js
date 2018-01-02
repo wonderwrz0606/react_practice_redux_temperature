6fcba614fb1053ab312af19b6923df2a28990244
Runze Wang <rwang@mettel.net>
2018-01-02 11:19:43 -0500

    Sparkline back

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import _ from 'lodash';
-// import {
-//   Sparklines,
-//   SparklinesLine,
-//   SparklinesReferenceLine
-// } from 'react-sparklines';
+import {
+  Sparklines,
+  SparklinesLine,
+  SparklinesReferenceLine
+} from 'react-sparklines';
 
 function average(data) {
   return _.round(_.sum(data)/data.length);
 }
 
-// const Chart = (props) => {
-//   return (
-//     <div>
-//       <Sparklines data={props.data} height={props.height} width={props.width} >
-//         <SparklinesLine color={props.color} />
-//         <SparklinesReferenceLine type="avg" />
-//       </Sparklines>
-//       <div title="unit">{average(props.data)} {props.units}</div>
-//     </div>
-//   );
-// }
-
 const Chart = (props) => {
   return (
     <div>
+      <Sparklines data={props.data} height={props.height} width={props.width} >
+        <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type="avg" />
+      </Sparklines>
       <div title="unit">{average(props.data)} {props.units}</div>
     </div>
   );
 }
+
+// const Chart = (props) => {
+//   return (
+//     <div>
+//       <div title="unit">{average(props.data)} {props.units}</div>
+//     </div>
+//   );
+// }
 
 export default Chart;
